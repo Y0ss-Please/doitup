a26ae45ad0e798fc357aeaef462b21c81ec2de85
Y0ss-Please <casey.elzinga@gmail.com>
2020-01-31 13:41:27 -0700

    Building javascript functionality

--- public/modules/DOM_manipulation.js
+++ public/modules/DOM_manipulation.js
@@ -1,5 +1,87 @@
 export function render(tasks, groups) {
-    tasks.forEach((e) => {
-        const html = '';
+    const activeGroup = getActiveGroup(groups);
+    renderTasks(tasks, activeGroup);
+    renderGroups(groups);
+}
+
+function renderTasks(tasks, activeGroup) {
+    const container = document.querySelector('.main');
+    container.innerHTML = `<div class="add-entry">+ New Task</div>`; // Clears the div
+    // Filter out tasks that are not assigned to the current group
+    let activeTasks = [];
+    if (activeGroup) {
+        tasks.forEach((e)=> {
+            if (e.group == activeGroup) {
+                activeTasks.push(e);
+            }
+        });
+    } else {
+        // if no group is active, allow all tasks
+        activeTasks = tasks;
+    }
+
+    if (activeTasks.length != 0) {
+        activeTasks.forEach((e) => {
+            const html ='<div class="checkbox-container">' +
+                            '<div class="checkbox">X</div>' +
+                        '</div>' +
+                        `<div class="title">${e.title}` +
+                            '<span class="tag"></span>' +
+                            '<hr>' +
+                        '</div>' +                        
+                        '<div class="description">' +
+                            `<p>${e.description}</p>` +
+                        '</div>' +
+                        '<div class="due-date">' +
+                            '<div class="month">Feb</div>' +
+                            '<div class="day">03</div>' +
+                            '<div class="time">12:00</div>' +
+                        '</div>';
+            const newEntry = document.createElement('div');
+            newEntry.className = 'entry';
+            newEntry.innerHTML = html;
+            container.appendChild(newEntry);
+        });
+    } else {
+        const html = 'It\'s totally empty...';
+        const newEntry = document.createElement('div');
+        newEntry.className = 'empty';
+        newEntry.textContent = html;
+        container.appendChild(newEntry);
+    }
+}
+
+function renderGroups(groups, activeGroup) {
+    const container = document.querySelector('.groups-container');
+    // Clear the div --
+    container.innerHTML = `<div class="header">
+                            Groups
+                            <div class="add-group">+</div>
+                            </div>`;
+    groups.forEach((e) => {
+        const newGroup = document.createElement('div');
+        newGroup.className = 'groups'
+        newGroup.textContent = capitalize(e.title);
+        newGroup.setAttribute('id', e.title);
+        if (e.active == true) {
+            newGroup.classList.add('active')
+            newGroup.textContent = newGroup.textContent + ' ⯈';
+        }
+        container.appendChild(newGroup);
     });
+
+}
+
+function getActiveGroup(groups) {
+    let active = ''
+    groups.forEach((e) => {
+        if (e.active) {
+            active = e.title;
+        }
+    });
+    return active;
+}
+
+function capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
 }
--- public/script.js
+++ public/script.js
@@ -8,13 +8,28 @@ let groups = [];
 const Task = function(title, description, group, date) {
     const id = taskID;
     const checked = false;
+    taskID++;
     return { title, description, group, date, id, checked };
 }
 
 const Group = function (title) {
     const id = groupID;
     const active = false;
+    groupID++;
     return { title, id, active };
+}
+
+function buildListeners() {
+    Array.from(document.querySelectorAll('.groups')).forEach((e) => {
+        document.addEventListener('click', setActiveGroup);
+    });
+}
+
+function setActiveGroup(e) {
+    groups.forEach((group) => {
+        (group.title == e.target.id)?group.active = true : group.active = false;
+    });
+    render(tasks,groups);
 }
 
 function dummyEntries() {
@@ -23,13 +38,13 @@ function dummyEntries() {
     const newestTask = new Task('Jubilate', 'HEYYYYY-EEEE-YAAAAY-EEE-YEAAAAAA-YEAH','play',Date.now());
     tasks.push(newTask,newerTask,newestTask);
     const newGroup = new Group('work');
+    // newGroup.active = true;
     const newerGroup = new Group('play');
+    newerGroup.active = true;
     groups.push(newGroup,newerGroup);
 }
 
 dummyEntries();
-
-console.log(tasks);
-
 render(tasks, groups);
+buildListeners();
 
